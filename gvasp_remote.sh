100bdf70b8548e70469bd691739fcc54caf6cb7d
Rasic2 <1051987201@qq.com>
2023-05-15 21:49:47 +0800

    complete the tool:v1

--- gvasp_remote.sh
+++ gvasp_remote.sh
@@ -19,15 +19,41 @@ do
 	fi
 done
 
-ip=`ls ${remote_file} | awk -F_ '{print $1}'`
-user=`ls ${remote_file} | awk -F_ '{print $2}'`
+ip=`echo ${remote_file} | awk -F_ '{print $1}'`
+user=`echo ${remote_file} | awk -F_ '{print $2}'`
+ssh_identify=${SrcDir}/keys/${ip}_${user}
 
 echo
-echo -e "${RED}  The input command is  : ${GREEN} $gvasp_command         ${RESET}"
-echo -e "${RED}  The remote address is : ${GREEN}  $ip (IP) $user (USER) ${RESET}"
+echo -e "----------------->${RED} GVasp Remote Process Tool ${RESET}<---------------------"
+echo
+echo -e "+-----------------------+-----------------------------------------+"
+printf  "|${RED}  input command       ${RESET} | ${GREEN} %20s                  ${RESET} |\n" "$gvasp_command"
+printf  "|${RED}  remote host         ${RESET} | ${GREEN} %12s (IP) %12s (USER) ${RESET} |\n" "$ip" "$user"
+echo -e "+-----------------------+-----------------------------------------+"
 
 WorkDir=`pwd`
 DirRemoveHOME=${WorkDir//$HOME}
 
-ssh $user@$ip -i ${ip}_${user} "echo \${HOME}${DirRemoveHOME};"
+# process the gvasp_command
+echo
+echo -e "${BOLD}#---> GVasp output${RESET}"
+eval $gvasp_command
+echo -e "${BOLD}<---# End GVasp output${RESET}"
+echo
+
+echo -e "${BOLD}#---> Mapping Directory:${RESET} ${BLUE}\$HOME${DirRemoveHOME}${RESET}"
+ssh $user@$ip -i ${ssh_identify} "echo \${HOME}${DirRemoveHOME} > ~/.gvasp_remote; mkdir -p \${HOME}${DirRemoveHOME};"
+scp -i ${ssh_identify} $user@$ip:~/.gvasp_remote . > /dev/null
+target_directory=`cat .gvasp_remote`
+rm -rf .gvasp_remote
+
+echo
+echo -e "${BOLD}#---> Transfer files${RESET}"
+echo
+scp -i ${ssh_identify} ./* $user@$ip:$target_directory
+echo
+echo -e "${BOLD}<---# End Transfer files${RESET}"
+
+ssh -i ${ssh_identify} $user@$ip "rm -rf ~/.gvasp_remote;"
+echo
 
